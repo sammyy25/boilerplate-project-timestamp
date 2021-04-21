6221cd23e1807f9c19b5a78da9e9ce22a11da7d9
sammyy25 <66947139+sammyy25@users.noreply.github.com>
2021-04-21 12:12:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/timestamp", function(req, res) {
+  const date = new Date();
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+})
+
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_str", function (req, res) {
+  const { date_str} = req.params;
+
+  let date = new Date(date_str);
+
+  if(date.toString() === "Invalid Date"){
+      date = new Date(parseInt(date_str));  
+  }
+
+  if(date.toString() ==="Invalid Date") {
+    return res.json({ error: "Invalid Date"});
+  }else {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+
 });
 
 
